1782d52c5d55e6a37264b86c25d63d11c9008c2a
lisabronwyn <e.bronwyn@gmail.com>
2017-05-24 15:06:42 -0700

    pushed exercises that are completed

---
+++ command-line/1.1-exercise-one.sh
@@ -0,0 +1,1 @@
+# By referring to Figure 5, identify the prompt, command, options, arguments, and cursor in each line of Figure 6.
---
+++ command-line/1.2-exercise-one.sh
@@ -0,0 +1,6 @@
+# Q: Write a command that prints out the string “hello, world”.
+
+# Extra credit: As in Listing 1, do it two different ways, both with and without using quotation marks.
+
+echo "hello, world"
+echo hello, world

e3c88fb8e7c3690649ef6db27991affcd403a62b
lisabronwyn <e.bronwyn@gmail.com>
2017-05-26 14:54:17 -0700

    added three exercises from bashscripting to project

---
+++ bash-scripting/multiply.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Create a simple script which will take two command line arguments and then multiply them together using each of the methods detailed above.
+# Pass in two nums
+
+let a = $1 \* $2
+echo $a
+
+expr $1 \* $2
+
+b=$(( $1 * $2 ))
+echo $b
+
+textVar="Hello World"
+echo ${#textVar}
